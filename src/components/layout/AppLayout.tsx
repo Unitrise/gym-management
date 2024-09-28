15a96796deb7f6716c1f35c59e7e352e94a0b78b
Unitrise <talhaviv1993@gmail.com>
2024-09-28 21:33:07 +0300

    asdasd

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import { Layout, Menu } from 'antd';
+// import {
+//   UserOutlined,
+//   AppstoreOutlined,
+//   FileDoneOutlined,
+// } from '@ant-design/icons';
+
+// const { Header, Content, Sider } = Layout;
+
+// const AppLayout: React.FC = ({ children }) => {
+//   return (
+//     <Layout style={{ minHeight: '100vh' }}>
+//       <Sider collapsible>
+//         <div className="logo" style={{ height: '32px', margin: '16px', background: 'rgba(255, 255, 255, 0.2)' }} />
+//         <Menu theme="dark" mode="inline">
+//           <Menu.Item key="1" icon={<UserOutlined />}>
+//             <Link to="/members">Members</Link>
+//           </Menu.Item>
+//           <Menu.Item key="2" icon={<AppstoreOutlined />}>
+//             <Link to="/inventory">Inventory</Link>
+//           </Menu.Item>
+//           <Menu.Item key="3" icon={<FileDoneOutlined />}>
+//             <Link to="/contracts">Contracts</Link>
+//           </Menu.Item>
+//         </Menu>
+//       </Sider>
+//       <Layout>
+//         <Header style={{ background: '#fff', padding: 0 }}>Gym Management</Header>
+//         <Content style={{ margin: '16px' }}>
+//           {children}
+//         </Content>
+//       </Layout>
+//     </Layout>
+//   );
+// };
+
+// export default AppLayout;
+export {}
